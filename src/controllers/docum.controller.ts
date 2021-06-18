8416bbb33ae42feed4c2ec4952cec277442188a9
VishalSingh28 <visusingh009@gmail.com>
2021-06-18 12:32:04 +0530

    FileUpload commit

--- src/controllers/docum.controller.ts
+++ src/controllers/docum.controller.ts
@@ -37,43 +37,43 @@ export class DocumController {
     content: {'application/json': {schema: getModelSchemaRef(Docu)}},
   })
 
-  // async fileUpload(
-  //   @requestBody.file()
-  //   request: Request,
-  //   @inject(RestBindings.Http.RESPONSE) response: Response,
-  // ): Promise<object> {
-  //   return new Promise<object>((resolve, reject) => {
-  //     this.handler(request, response, (err: unknown) => {
-  //       if (err) reject(err);
-  //       else {
-  //         resolve(DocumController.getFilesAndFields(request));
-  //       }
-  //     });
-  //   });
-  // }
-  // /**
-  //  * Get files and fields for the request
-  //  * @param request - Http request
-  //  */
-  // private static getFilesAndFields(request: Request) {
-  //   const uploadedFiles = request.files;
-  //   const mapper = (f: globalThis.Express.Multer.File) => ({
-  //     fieldname: f.fieldname,
-  //     originalname: f.originalname,
-  //     encoding: f.encoding,
-  //     mimetype: f.mimetype,
-  //     size: f.size,
-  //   });
-  //   let files: object[] = [];
-  //   if (Array.isArray(uploadedFiles)) {
-  //     files = uploadedFiles.map(mapper);
-  //   } else {
-  //     for (const filename in uploadedFiles) {
-  //       files.push(...uploadedFiles[filename].map(mapper));
-  //     }
-  //   }
-  //   return {files, fields: request.body};
-  // }
+  async fileUpload(
+    @requestBody.file()
+    request: Request,
+    @inject(RestBindings.Http.RESPONSE) response: Response,
+  ): Promise<object> {
+    return new Promise<object>((resolve, reject) => {
+      this.handler(request, response, (err: unknown) => {
+        if (err) reject(err);
+        else {
+          resolve(DocumController.getFilesAndFields(request));
+        }
+      });
+    });
+  }
+  /**
+   * Get files and fields for the request
+   * @param request - Http request
+   */
+  private static getFilesAndFields(request: Request) {
+    const uploadedFiles = request.files;
+    const mapper = (f: globalThis.Express.Multer.File) => ({
+      fieldname: f.fieldname,
+      originalname: f.originalname,
+      encoding: f.encoding,
+      mimetype: f.mimetype,
+      size: f.size,
+    });
+    let files: object[] = [];
+    if (Array.isArray(uploadedFiles)) {
+      files = uploadedFiles.map(mapper);
+    } else {
+      for (const filename in uploadedFiles) {
+        files.push(...uploadedFiles[filename].map(mapper));
+      }
+    }
+    return {files, fields: request.body};
+  }
 
 
 
